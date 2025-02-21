d7c9afc770b18ac0abaf44c8b87d2ca39603b2b9
AminCharepoo <134405766+AminCharepoo@users.noreply.github.com>
2025-02-20 21:13:45 -0500

    Added Navmesh
    Added navmesh and navmesh agent to all the enemies and player. Also fixed colliders on enemies

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyAI : MonoBehaviour
+{
+    public Transform player;
+    public float detectionRange = 10f;
+    private navmeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
