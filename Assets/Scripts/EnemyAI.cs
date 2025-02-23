8cfaaade4752360d09e917cc5aba8e35217988de
AminCharepoo <134405766+AminCharepoo@users.noreply.github.com>
2025-02-22 22:36:30 -0500

    Added enemies chasing
    Bugs:
    
    still chases player even if they behind wall

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,16 +6,58 @@ public class EnemyAI : MonoBehaviour
 {
     public Transform player;
     public float detectionRange = 10f;
+    public float stoppingDistance = 2f;
     private NavMeshAgent agent;
-    // Start is called before the first frame update
+    public LayerMask obstacleMask;
+    [SerializeField] private float distanceToPlayer;
+
     void Start()
     {
-
+        agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        distanceToPlayer = Vector3.Distance(transform.position, player.position);
 
+
+        if (distanceToPlayer <= detectionRange)
+        {
+            if (distanceToPlayer <= stoppingDistance && HaslineOfSight())
+            {
+                agent.isStopped = true;
+                agent.ResetPath();
+            }
+            else
+            {
+                agent.isStopped = false;
+                agent.SetDestination(player.position);
+            }
+
+        }
+        else
+        {
+            agent.isStopped = true;
+            agent.ResetPath();
+        }
     }
+
+    private bool HaslineOfSight()
+    {
+        RaycastHit hit;
+        Vector3 startRayPosition = transform.position + Vector3.up * 1.5f;
+        Vector3 directionToPlayer = (player.position - startRayPosition).normalized;
+
+        Debug.DrawRay(startRayPosition, directionToPlayer * detectionRange, Color.red, 0.1f);
+
+
+        if (Physics.Raycast(startRayPosition, directionToPlayer, out hit, detectionRange, obstacleMask))
+        {
+            Debug.Log("Ray hit: " + hit.transform.name);
+            return hit.transform == player;
+        }
+        return false;
+    }
+
 }
