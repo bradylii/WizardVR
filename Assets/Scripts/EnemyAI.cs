207656d72f2ca395956c02d3e02a565ad4a8320e
AminCharepoo <134405766+AminCharepoo@users.noreply.github.com>
2025-02-23 23:37:03 -0500

    Removed faulty code in enemyAI

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -8,7 +8,6 @@ public class EnemyAI : MonoBehaviour
     public float detectionRange = 10f;
     public float stoppingDistance = 2f;
     private NavMeshAgent agent;
-    public LayerMask obstacleMask;
     [SerializeField] private float distanceToPlayer;
 
     void Start()
@@ -24,7 +23,7 @@ public class EnemyAI : MonoBehaviour
 
         if (distanceToPlayer <= detectionRange)
         {
-            if (distanceToPlayer <= stoppingDistance && HaslineOfSight())
+            if (distanceToPlayer <= stoppingDistance)
             {
                 agent.isStopped = true;
                 agent.ResetPath();
@@ -43,21 +42,5 @@ public class EnemyAI : MonoBehaviour
         }
     }
 
-    private bool HaslineOfSight()
-    {
-        RaycastHit hit;
-        Vector3 startRayPosition = transform.position + Vector3.up * 1.5f;
-        Vector3 directionToPlayer = (player.position - startRayPosition).normalized;
-
-        Debug.DrawRay(startRayPosition, directionToPlayer * detectionRange, Color.red, 0.1f);
-
-
-        if (Physics.Raycast(startRayPosition, directionToPlayer, out hit, detectionRange, obstacleMask))
-        {
-            Debug.Log("Ray hit: " + hit.transform.name);
-            return hit.transform == player;
-        }
-        return false;
-    }
 
 }
