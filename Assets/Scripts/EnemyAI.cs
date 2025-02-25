39a91ae218faa97aada40504f43db5b405c0623e
Osvetamp4 <82180979+Osvetamp4@users.noreply.github.com>
2025-02-24 19:51:17 -0500

    Started Enemy State Machine
    Started working on a state machine for Bad guy AI.
    
    Currently has an enum class EnemyState and little if statements determining what to do depending on which state the enemy is in.
    
    Currently unfinished and does not work.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,12 +17,19 @@ public class EnemyAI : MonoBehaviour
     public Transform player;
     public float detectionRange = 10f;
     public float stoppingDistance = 2f;
+    
     private NavMeshAgent agent;
     [SerializeField] private float distanceToPlayer;
+
+
+    public EnemyState currentState = EnemyState.Wander;
+    public float dRadius = 6969;
+    public Vector3 originalWanderPoint;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        originalWanderPoint = transform.position;
     }
 
 
@@ -29,27 +37,42 @@ public class EnemyAI : MonoBehaviour
     {
         distanceToPlayer = Vector3.Distance(transform.position, player.position);
 
+        if (PlayerDetected()) currentState = EnemyState.Pursue;
+
+
+
+
+        //if (distanceToPlayer <= detectionRange)
+        //{
+        //    if (distanceToPlayer <= stoppingDistance)
+        //    {
+        //        agent.isStopped = true;
+        //        agent.ResetPath();
+        //    }
+        //    else
+        //    {
+        //        agent.isStopped = false;
+        //        agent.SetDestination(player.position);
+        //    }
+
+        //}
+        //else
+        //{
+        //    agent.isStopped = true;
+        //    agent.ResetPath();
+        }
+
+    Boolean PlayerDetected()
+    {
+        
 
         if (distanceToPlayer <= detectionRange)
         {
-            if (distanceToPlayer <= stoppingDistance)
-            {
-                agent.isStopped = true;
-                agent.ResetPath();
-            }
-            else
-            {
-                agent.isStopped = false;
-                agent.SetDestination(player.position);
-            }
+            return true;
 
         }
-        else
-        {
-            agent.isStopped = true;
-            agent.ResetPath();
-        }
-    }
+        return false;
+}
 
 
 }
