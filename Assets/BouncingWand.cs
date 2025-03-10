8c687625493904095fbed556df6a929f281c4e77
PagePatterson <patterson_page915@outlook.com>
2025-03-10 17:43:21 -0400

    add spaceship, space sky box, bouncing logic, text, lobby area

---
+++ Assets/BouncingWand.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Transform))]
+public class BouncingWand : MonoBehaviour
+{
+    private Vector3 objectPos;
+    private float changingValue = 1;
+    private float maxPercent = 1.2f;
+    private float minPercent = 0.8f;
+    private float speed = 1.5f;
+    private void Start()
+    {
+        objectPos = this.transform.position;
+    }
+
+    private void Update()
+    {
+        bounceObject();
+    }
+
+    private void bounceObject()
+    {
+        float t = (Mathf.Sin(Time.time * speed) + 1) / 2;
+        changingValue = Mathf.Lerp(minPercent, maxPercent, t);
+        transform.position = new Vector3(objectPos.x, (objectPos.y * changingValue), objectPos.z);
+    }
+}
