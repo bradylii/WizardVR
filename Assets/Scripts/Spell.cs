c57c0fbeb1310f627882c7974c74ee549f23043e
PagePatterson <patterson_page915@outlook.com>
2025-03-14 17:38:23 -0400

    fix spell angle, add size component to spell

--- Assets/Scripts/Spell.cs
+++ Assets/Scripts/Spell.cs
@@ -4,16 +4,31 @@ public class Spell : MonoBehaviour
 {
     [SerializeField] private float lifetime = 2;
     [SerializeField] private float velocity = 50;
+    [SerializeField] private float sizePercentage = 1;
 
     private Rigidbody rb;
     private Vector3 launchForce = Vector3.right;
+    private Vector3 originalSize;
     private float timer = 0;
-    
+    private bool hasLaunched = false;
+
+
     void Start()
     {
+        SetOriginalSizeValue();
+        UpdateSize();
         if (rb == null)
             rb = GetComponent<Rigidbody>();
         LaunchObject();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!hasLaunched && rb != null)
+        {
+            LaunchObject();
+            hasLaunched = true;
+        }
     }
 
     private void Update()
@@ -27,7 +42,18 @@ public class Spell : MonoBehaviour
 
     private void LaunchObject()
     {
-        launchForce = Vector3.right * velocity;
+        if (rb == null) return;
+        launchForce = transform.right * velocity;
         rb.AddForce(launchForce, ForceMode.Impulse);
+    }
+
+    private void SetOriginalSizeValue()
+    {
+        originalSize = transform.localScale;
+    }
+
+    private void UpdateSize()
+    {
+        this.transform.localScale = originalSize * sizePercentage;
     }
 }
