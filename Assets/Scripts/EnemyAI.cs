1a5450c356a5515b65e17a4be102f5087916103c
Osvetamp4 <82180979+Osvetamp4@users.noreply.github.com>
2025-03-17 19:58:01 -0400

    Updated EnemyAI.cs
    We're gonna have to add a way to auto caculate paths

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -23,21 +23,38 @@ public class EnemyAI : MonoBehaviour
 
 
     public EnemyState currentState = EnemyState.Wander;
-    public float dRadius = 6969;
+    public int dRadius = 500;
     public Vector3 originalWanderPoint;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        originalWanderPoint = transform.position;
+        originalWanderPoint = transform.position;//add a raycast to snap to the ground
     }
 
 
     void Update()
     {
         distanceToPlayer = Vector3.Distance(transform.position, player.position);
-
+        
+        //this section modifies the enemy state
         if (PlayerDetected()) currentState = EnemyState.Pursue;
+
+
+        //this section determines what to do in any given state;
+
+        switch (currentState)
+        {
+            case == EnemyState.Wander:
+                System.Random random = new System.Random();
+                int random_number_x = random.Next( ( (int) originalWanderPoint.x) - dRadius,  ((int) originalWanderPoint.x) + 1 + dRadius);
+                int random_number_z = random.Next(((int)originalWanderPoint.z) - dRadius, ((int)originalWanderPoint.z) + 1 + dRadius);
+                Vector3 generated_point = new Vector3(random_number_x, originalWanderPoint.y, random_number_z);
+
+                if (Vector3.Distance(generated_point,transform.position) <= dRadius && agent.CalculatePath(generated_point,)
+                //agent.CalculatePath will return true if there is a path, false if no path exists.
+                //Physics.SphereCast() returns every object in that sphere.
+        }
 
 
 
