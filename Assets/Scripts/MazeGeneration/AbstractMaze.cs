aefd2fb58af8c9e7dcdb983ef5c3214ea7ad29cc
kelvinx24 <113376783+kelvinx24@users.noreply.github.com>
2025-03-19 00:33:36 -0400

    KAN-16: Maze generation with Kruskal's Algorithm
    - Generate maze of specified size with KruskalMaze script (will run on start for now)
    
    - Added MazeGen scene for example
    - Added various supporting scripts for maze representation and generation

---
+++ Assets/Scripts/MazeGeneration/AbstractMaze.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// AbstractMaze is an abstract class that implements the MazeWorld interface. 
+// It provides a basic implementation of the GetCell, GetHeight, and GetWidth methods. 
+// It also provides a List of Lists of MazeCells to store the cells of the maze. 
+// The Generate and Build methods are declared as abstract, so they must be implemented by subclasses of AbstractMaze.
+public abstract class AbstractMaze : MonoBehaviour, MazeWorld
+{
+    [SerializeField]
+    protected int width = 10;
+
+    [SerializeField]
+    protected int height = 10;
+
+    protected List<List<MazeCell>> cells = new List<List<MazeCell>>();
+
+    public abstract void Generate();
+    public abstract GameObject Build();
+
+    public MazeCell GetCell(int x, int y)
+    {
+        if (x < 0 || y < 0 || x >= width || y >= height)
+        {
+            return null;
+        }
+        else
+        {
+            return cells[x][y];
+        }
+    }
+
+
+    public int GetHeight()
+    {
+        return height;
+    }
+
+    public int GetWidth()
+    {
+        return width;
+    }
+
+    
+}
