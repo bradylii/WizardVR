f735b941f143034cb712476662e65f5aefe810a7
Brady Li <67674462+bradylii@users.noreply.github.com>
2025-04-07 20:14:15 -0400

    stuff larry did

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -2,6 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+
+
+
+
+public enum EnemyState
+{
+    Wander_init = 0,
+    Wander_to_Path = 1,
+    Pursue = 2,
+    Search_init = 3,
+    Search_to_Path = 4
+}
 public class EnemyAI : MonoBehaviour
 {
     // How much damage enemy does
@@ -28,10 +40,35 @@ public class EnemyAI : MonoBehaviour
     private bool playerSeen = false;
     private Vector3 lastKnownPlayerPosition;
 
+    //variables for Search_init and Search_To_Path timers
+    private float timerDuration = 10f;
+    public float currentTime = 0f;
+
+    //manages the current state in the state machine
+    public EnemyState currentState = EnemyState.Wander_init;
+    
+
+    //variables for the different wanderpoints and destination points
+    public Vector3 originalWanderPoint;
+    public Vector3 newWanderPoint;
+    public Vector3 destinationPoint;
+    int dRadius = 4;
+
+
+    //variable to judge how far enemy can be from a destination point
+    public float margin_of_difference;
+
+
     void Start()
     {
         // Navmesh init
         agent = GetComponent<NavMeshAgent>();
+
+
+        originalWanderPoint = transform.position;//add a raycast to snap to the ground
+        originalWanderPoint.y = (float)0.58;
+
+
         playerInfo = GameObject.Find("Game Manager")?.GetComponent<Player>();
         if (player == null)
         {
@@ -42,39 +79,116 @@ public class EnemyAI : MonoBehaviour
 
     void Update()
     {
+        distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        //this section modifies the enemy state
+        if (PlayerDetected())
+        {
+            currentState = EnemyState.Pursue;
+        }
+        else if (currentState == EnemyState.Pursue)
+        {
+            newWanderPoint = path.corners[path.corners.Length - 1];
+            currentState = EnemyState.Search_init;//this is supposed to go EnemyState.Search_init but for now we will go to Wander_init
+        }
+
+
+        //this section determines what to do in any given state;
+
+        switch (currentState)
+        {
+            case EnemyState.Wander_init:
+                currentTime = 0f;
+
+                //randomly select a point in a radius around the wanderpoint
+                System.Random random = new System.Random();
+                int random_number_x = random.Next(((int)originalWanderPoint.x) - dRadius, ((int)originalWanderPoint.x) + 1 + dRadius);
+                int random_number_z = random.Next(((int)originalWanderPoint.z) - dRadius, ((int)originalWanderPoint.z) + 1 + dRadius);
+                Vector3 generated_point = new Vector3(random_number_x, originalWanderPoint.y, random_number_z);
+
+                path = new NavMeshPath();
+                if (Vector3.Distance(generated_point, originalWanderPoint) <= dRadius && agent.CalculatePath(generated_point, path))
+                {
+                    destinationPoint = generated_point;
+                    //currentState = EnemyState.Wander_to_Path;
+                    //transform.position = generated_point;
+                    currentState = EnemyState.Wander_to_Path;
+                    agent.SetPath(path);
+                }
+                break;
+            case EnemyState.Wander_to_Path:
+                if (agent.remainingDistance == 0 || agent.velocity == new Vector3(0, 0, 0))
+                {
+                    currentState = EnemyState.Wander_init;
+                }
+                break;
+            case EnemyState.Pursue:
+                currentTime = 0f;
+                if (Vector3.Distance(destinationPoint, player.position) > margin_of_difference)
+                {
+                    path = new NavMeshPath();
+                    agent.CalculatePath(player.position, path);
+                    agent.SetPath(path);
+                }
+                break;
+            case EnemyState.Search_init:
+                if (addDeltaTime() == true)
+                {
+                    currentState = EnemyState.Wander_init;
+                }
+                //newWanderPoint = path.corners[path.corners.Length - 1];//Create a new temporary wanderpoint to circle around.
+                System.Random random1 = new System.Random();
+                int random_number_x1 = random1.Next(((int)newWanderPoint.x) - dRadius, ((int)newWanderPoint.x) + 1 + dRadius);
+                int random_number_z1 = random1.Next(((int)newWanderPoint.z) - dRadius, ((int)newWanderPoint.z) + 1 + dRadius);
+                Vector3 generated_point1 = new Vector3(random_number_x1, newWanderPoint.y, random_number_z1);
+
+                path = new NavMeshPath();
+
+
+                if (Vector3.Distance(generated_point1, newWanderPoint) <= dRadius && agent.CalculatePath(generated_point1, path))
+                {
+                    destinationPoint = generated_point1;
+                    //currentState = EnemyState.Wander_to_Path;
+                    //transform.position = generated_point;
+                    currentState = EnemyState.Search_to_Path;
+                    agent.SetPath(path);
+                }
+                break;
+            case EnemyState.Search_to_Path:
+                if (addDeltaTime() == true)
+                {
+                    currentState = EnemyState.Wander_init;
+                }
+                if (agent.remainingDistance == 0 || agent.velocity == new Vector3(0, 0, 0))
+                {
+                    currentState = EnemyState.Search_init;
+                }
+                break;
+            default:
+
+                break;
+
+        }
+    }
+
+    bool addDeltaTime()
+    {
+        currentTime += Time.deltaTime;
+        if (currentTime >= timerDuration)
+        {
+            currentTime = 0f;
+            return true;
+        }
+        return false;
+    }
+
+    bool PlayerDetected()
+    {
         if (isInRange() && isFacingPlayer() && inLineOfSight()) // check if player is visible
         {
-            playerSeen = true;
-            lastKnownPlayerPosition = player.position;
-            RotateTowardsPlayer(); // rotate to player 
-
-            if (distanceToPlayer <= stoppingDistance) // if enemy is close to player stop movement
-            {
-                agent.isStopped = true;
-                if (!isDamagingPlayer) // Only start coroutine if it's not already running
-                {
-                    StartCoroutine(DamagePlayerOverTime());
-                }
-                agent.ResetPath();
-            }
-            else
-            {
-                agent.isStopped = false;
-                agent.SetDestination(player.position); // if not go to player
-            }
+            return true;
         }
-        else if (playerSeen) // if player out of vision but enemy has seen them
-        {
-            agent.isStopped = false;
-            agent.SetDestination(lastKnownPlayerPosition); // go to last known position
-
-            if (Vector3.Distance(transform.position, lastKnownPlayerPosition) <= stoppingDistance) // if enemy is close to last known position, stop
-            {
-                agent.isStopped = true;
-                agent.ResetPath();
-                playerSeen = false;
-            }
-        }
+        return false;
     }
 
     private IEnumerator DamagePlayerOverTime()
