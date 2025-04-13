83fa2894d2ad67e5adb0cbe4cc1373f48cde6fa2
AminCharepoo <134405766+AminCharepoo@users.noreply.github.com>
2025-04-13 15:12:10 -0400

    Fixed spell collision

--- Assets/Scripts/Weapons/Spell.cs
+++ Assets/Scripts/Weapons/Spell.cs
@@ -62,11 +62,11 @@ public class Spell : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if ("weapon" == other.gameObject.tag)
-        {
-            Debug.Log("[SPELL] hit itself");
-            return;
-        }
-        Destroy(gameObject);
+        // if ("weapon" == other.gameObject.tag)
+        // {
+        //     Debug.Log("[SPELL] hit itself");
+        //     return;
+        // }
+        // Destroy(gameObject);
     }
 }
