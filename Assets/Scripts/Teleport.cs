5ede71b3709923bbcc2c017c7d3e4778f4f4bded
AminCharepoo <134405766+AminCharepoo@users.noreply.github.com>
2025-04-17 17:20:46 -0400

    Tried teleporting into pit

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -9,6 +9,9 @@ public class Teleport : MonoBehaviour
     public float z;
 
     public bool teleport = false;
+
+    Rigidbody rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,8 +23,34 @@ public class Teleport : MonoBehaviour
     {
         if (teleport)
         {
+            GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = false;
+            Debug.Log("[TELEPORT] Navmesh disabled");
             transform.position = new Vector3(x, y, z);
+            GetComponent<Rigidbody>().isKinematic = false;
+            GetComponent<Rigidbody>().useGravity = true;
+            Debug.Log("[TELEPORT] Rigidbody altered");
+            Debug.Log("[TELEPORT] Teleported");
             teleport = false;
+
+            GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = true;
+            Debug.Log("[TELEPORT] Navmesh enabled");
+
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = true;
+            Debug.Log("[TELEPORT] Navmesh enabled");
+            GetComponent<Rigidbody>().isKinematic = true;
+            GetComponent<Rigidbody>().useGravity = false;
+            Debug.Log("[TELEPORT] Rigidbody altered");
+
         }
     }
 }
+
+
