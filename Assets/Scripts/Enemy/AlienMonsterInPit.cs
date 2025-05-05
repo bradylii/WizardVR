1dcb06c32800b2063ba5881ea147ac5f83a8f708
AminCharepoo <134405766+AminCharepoo@users.noreply.github.com>
2025-05-04 20:05:15 -0400

    Fixed boulders

--- Assets/Scripts/Enemy/AlienMonsterInPit.cs
+++ Assets/Scripts/Enemy/AlienMonsterInPit.cs
@@ -1,35 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AlienMonsterInPit : MonoBehaviour
 {
+    #region References
+    [Header("References")]
     public Transform player;
     public Animator animator;
 
-    private UnityEngine.AI.NavMeshAgent agent;
+    private NavMeshAgent agent;
+    #endregion
 
+    #region Movement and Combat
+    [Header("Movement and Combat")]
     public float offset = 2f;
-
     [SerializeField] private float velocity;
-
     [SerializeField] private bool chasingPlayer = false;
-
+    #endregion
 
     private void Start()
     {
         animator = GetComponent<Animator>();
 
-        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>();
     }
 
     private void Update()
     {
         if (chasingPlayer)
         {
+            // Update animation based on speed
             velocity = agent.velocity.magnitude;
             animator.SetFloat("speed", velocity);
 
+            // if close enough to target, attack
             if (!agent.pathPending)
             {
                 float distanceToTarget = Vector3.Distance(agent.destination, agent.transform.position);
@@ -48,6 +54,7 @@ public class AlienMonsterInPit : MonoBehaviour
             player = GameObject.FindGameObjectWithTag("Player").transform;
         }
 
+        // Destination is offset from player 
         if (player != null)
         {
             Vector3 targetPosition = new Vector3(player.position.x, player.position.y, player.position.z + offset);
